b2fa65e8ceb7e1778c2ee5a05ed5d3533ee7b1dc
Armilion <valentin.andre94@gmail.com>
2022-01-16 12:37:37 +0100

    Final commit

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import AddChatScreen from './screens/AddChatScreen';
+import ChatScreen from './screens/ChatScreen';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: 'white' },
+  headerTintColor: 'white',
+  gestureEnabled:true
+}
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator /*initialRouteName="Home"*/ screenOptions={globalScreenOptions}>
+        <Stack.Screen options={{title:'Login'}} name="Login" component={LoginScreen} />
+        <Stack.Screen options={{title:'Register'}} name='Register' component={RegisterScreen} />
+        <Stack.Screen options={{title:'Home'}} name='Home' component={HomeScreen} />
+        <Stack.Screen options={{title:'AddChat'}} name='AddChat' component={AddChatScreen} />
+        <Stack.Screen options={{title:'Chat'}} name='Chat' component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
